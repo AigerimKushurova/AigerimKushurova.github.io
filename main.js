b039b4c2af633720152ecda2960c92917ca22c36
Айгерим Кушурова <aigerimkusurova@MacBook-Air-2.local>
2022-12-04 22:22:49 +0600

    Project

--- main.js
+++ main.js
@@ -2,6 +2,30 @@
 
 ////////////////////////////////////////////////
 
+document.getElementById("h_op").onmouseover = function() {mouseOver()};
+document.getElementById("h_op").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+  document.getElementById("h_op").style.color = "#612B98";
+}
+
+function mouseOut() {
+  document.getElementById("h_op").style.color = "black";
+}
+
+let web_icon=document.createElement("link");
+let head= document.getElementById("webhead");
+head.appendChild(web_icon);
+
+function setAttributes(el, attrs) {
+     
+     for(var key in attrs) {
+        el.setAttribute(key, attrs[key]);
+     }
+}
+setAttributes(web_icon, {"rel": "shortcut icon", "href": "../images/web_icon.png", "type":"image/png"});
+
+
 
 
 const social= document.getElementById("social");
@@ -13,15 +37,19 @@ let date_vol4 = document.createElement("p");
 let date_vol5 = document.createElement("p");
 
 date_vol1.innerHTML ="2018";
-date_vol1.style.backgroundColor="#FDD1D2";
+
+date_vol1.style.verticalAlign="bottom";
+
+
+
 date_vol2.innerHTML ="2018";
-date_vol2.style.backgroundColor="#FDD1D2";
+
 date_vol3.innerHTML ="2019";
-date_vol3.style.backgroundColor="#FDD1D2";
+
 date_vol4.innerHTML ="2019";
-date_vol4.style.backgroundColor="#FDD1D2";
+
 date_vol5.innerHTML ="2022";
-date_vol5.style.backgroundColor="#FDD1D2";
+
 
 document.querySelector('.V1').appendChild(date_vol1);
 document.querySelector('.V2').appendChild(date_vol2);
@@ -29,21 +57,17 @@ document.querySelector('.V3').appendChild(date_vol3);
 document.querySelector('.V4').appendChild(date_vol4);
 document.querySelector('.V5').appendChild(date_vol5);
 
-let up_img= document.createElement('img');
-up_img.setAttribute("id","myBtn");
-up_img.setAttribute("src","images/up.png");
-up_img.style.width="8vh";
-up_img.style.marginLeft="90%";
 
 
-up_img.addEventListener(
-"click",
-function (event) {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-);
-document.body.appendChild(up_img);
+
+
+
+
+
+
+
+
+
 
 
 
